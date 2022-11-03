6b8af02f6e445d4a31e4eb91118e1eeea35525db
alexandrtrubkin <trubkin10@gmail.com>
2022-11-03 20:17:25 +0300

    3. Типы данных

--- Module_1.Base_JavaScript/may-2/3-task/3/index.js
+++ Module_1.Base_JavaScript/may-2/3-task/3/index.js
@@ -1,17 +1,31 @@
 let ageOfPerson1 = 18;
 let ageOfPerson2 = "20";
+ageOfPerson2 = ageOfPerson1;
+console.log(ageOfPerson2, typeof ageOfPerson2);
 
 let nameOfAnimal1 = null;
 let nameOfAnimal2 = "Charlie";
+nameOfAnimal2 = nameOfAnimal1;
+console.log(nameOfAnimal2, typeof nameOfAnimal2);
 
+console.log("Переменную с const переопределить нельзя");
 const bestProgrammingLanguage1 = "JavaScript";
 const bestProgrammingLanguage2 = "Java";
+console.log("Значение переменной bestProgrammingLanguage1 равно " + bestProgrammingLanguage1 + " а ее тип " + typeof bestProgrammingLanguage1);
+console.log("Значение переменной bestProgrammingLanguage2 равно " + bestProgrammingLanguage2 + " а ее тип " + typeof bestProgrammingLanguage2);
 
 let initialValue1;
 let initialValue2 = 0;
+initialValue2 = initialValue1;
+console.log(initialValue2, typeof initialValue2);
 
-var isJavaScriptProgrammer1 = true;
-var isJavaScriptProgrammer2 = false;
+console.log("Переменная var является устаревшей");
+let isJavaScriptProgrammer1 = true;
+let isJavaScriptProgrammer2 = false;
+isJavaScriptProgrammer2 = isJavaScriptProgrammer1;
+console.log(isJavaScriptProgrammer2, typeof isJavaScriptProgrammer2);
 
 let helloText1 = "Hello!";
 let helloText2 = "Привет!";
+helloText2 = helloText1;
+console.log(helloText2, typeof helloText2);
