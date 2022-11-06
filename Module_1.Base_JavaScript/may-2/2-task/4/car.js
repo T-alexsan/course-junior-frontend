7a7ef40934c4bba5e77b3edb586e6576f3ad7bd5
alexandrtrubkin <trubkin10@gmail.com>
2022-11-06 20:47:33 +0300

    2. Синтаксис

--- Module_1.Base_JavaScript/may-2/2-task/4/car.js
+++ Module_1.Base_JavaScript/may-2/2-task/4/car.js
@@ -3,11 +3,11 @@ let maxCarSpeed = '200';
 let carOwner = 'Aleksandr';
 
 alert(`У машины ${carName} изменилась скорость (Смотри в консоль)`);
-let NewmaxCarSpeed = '220';
+const NewmaxCarSpeed = '220';
 console.log('Новая скорость машины ' + NewmaxCarSpeed);
 
 alert(`У машины ${carName} сменился водитель (Смотри в консоль)`);
-let NewcarOwner = 'Mordva';
+const NewcarOwner = 'Mordva';
 console.log(`Новая имя водителя ${carOwner}`);
 
 /**alert(carName);
