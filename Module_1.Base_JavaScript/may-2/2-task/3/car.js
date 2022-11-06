7d4d0d5f0f951e030f535dfe407ed9b6a854948f
alexandrtrubkin <trubkin10@gmail.com>
2022-11-06 20:31:13 +0300

    2. Синтаксис

--- Module_1.Base_JavaScript/may-2/2-task/3/car.js
+++ Module_1.Base_JavaScript/may-2/2-task/3/car.js
@@ -1,6 +1,6 @@
-const carName = ('BMW');
-let maxCarSpeed = ('200');
-let carOwner = ('Aleksandr');
+const carName = 'BMW';
+let maxCarSpeed = '200';
+let carOwner = 'Aleksandr';
 
 alert(carName);
 alert(maxCarSpeed);
