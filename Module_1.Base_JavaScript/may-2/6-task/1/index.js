18838a1ddd32aab11eed8a494364bf07267b792d
alexandrtrubkin <trubkin10@gmail.com>
2022-11-10 10:18:36 +0300

    6. Введение в основы JavaScript

--- Module_1.Base_JavaScript/may-2/6-task/1/index.js
+++ Module_1.Base_JavaScript/may-2/6-task/1/index.js
@@ -2,21 +2,30 @@ const existingUserLogin = 'user';
 const existingUserPassword = 123;
 
 let userLogin = prompt('Введите логин').trim();
-if (userLogin === existingUserLogin){
-    console.log('Пользователь найден')
-    alert('Пользователь найден, введите пароль!')
-} else {
-    console.log('Пользователь не найден')
-    alert('Пользователь не найден!')
-}
+// if (userLogin === existingUserLogin){
+//     console.log('Пользователь найден')
+//         //alert('Пользователь найден, введите пароль!')
+// } else {
+//     console.log('Пользователь не найден')
+//     //alert('Пользователь не найден!')
+// }
 
 let userPassword = prompt('Введите пароль').trim();
-if (Number(userPassword) === existingUserPassword){
-    console.log('Пароль верный')
-    alert('Пароль верный')
+// if (Number(userPassword) === existingUserPassword){
+//     console.log('Пароль верный')
+//     //alert('Пароль верный')
+// } else {
+//     console.log('Пароль не верный!')
+//     //alert('Пароль не верный!')
+// }
+
+
+if (userLogin === existingUserLogin && Number(userPassword) === existingUserPassword) {
+    console.log('Авторизация успешная');
+    alert('Авторизация успешная');
 } else {
-    console.log('Пароль не верный!')
-    alert('Пароль не верный!')
+    console.log('Введенный логин или пароль, неверный');
+    alert('Введенный логин или пароль, неверный');
 }
 
 
