99a1d56b9b2dc905d98fff5fef475a2ef5cb3664
alexandrtrubkin <trubkin10@gmail.com>
2022-11-14 23:01:10 +0300

    8. Функции

--- Module_1.Base_JavaScript/may-2/6-task/5/index.js
+++ Module_1.Base_JavaScript/may-2/6-task/5/index.js
@@ -1,8 +1,15 @@
-let a = 1;
-let b = 100;
-let sum = 0;
-while(a <= b){
-    sum+=a;
-    a++;
+let sum = 0
+for (let i = 0; i <= 100; i++) {
+    sum += i
 }
 console.log(sum)
+
+
+// let a = 1;
+// let b = 100;
+// let sum = 0;
+// while(a <= b){
+//     sum+=a;
+//     a++;
+// }
+// console.log(sum)
